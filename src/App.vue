e6a43b5a0c6830d10cce3278b8bf6ea2efa5fe1a
Nantheshprabu <nanthesh2000@gmail.com>
2021-07-02 14:35:19 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <h1>{{ title }}</h1>
+  <p>Welcome!!</p>
+  <div v-if="showModal">
+    <Modal :header="header" theame="sale" @closeing="toggleModal">
+      <template v-slot:links>
+        <a href="www.amazon.in">amazon</a>
+      </template>
+      <h1>Giveaway Offer</h1>
+    </Modal>
+  </div>
+
+  <button @click="toggleModal">Open Modal</button>
+</template>
+
+<script>
+import Modal from "./components/Modal.vue";
+
+export default {
+  components: { Modal },
+  data() {
+    return {
+      title: "My first Vue App",
+      header: "Sale is here",
+      showModal: false,
+    };
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name);
+      this.$refs.name.classList.add("active");
+      this.$refs.name.focus();
+    },
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal" :class="{ sale: theame === 'sale' }">
+      <slot></slot>
+      <div>
+        <slot name="links"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["theame"],
+
+  methods: {
+    closeModal() {
+      this.$emit("closeing");
+    },
+  },
+};
+</script>
+
+<style>
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: white;
+}
+.backdrop {
+  top: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+.modal.sale {
+  background: red;
+  color: white;
+}
+</style>
